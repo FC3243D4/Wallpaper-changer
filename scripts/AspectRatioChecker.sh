a2b3a94736f9415633c5b81b90ec91feb06cc3d3
FC3243D4 <colacicchi4@gmail.com>
2026-02-24 10:36:38 +0100

    improved

--- scripts/AspectRatioChecker.sh
+++ scripts/AspectRatioChecker.sh
@@ -3,129 +3,33 @@
 # $1 = width
 # $2 = height
 
-#get aspect ratio multiplied by 1000 to check for decimal
-value10="$((90 * $1/$2))"
-dec=${value10: -1}
+# Calculate actual aspect ratio
+actual_ratio=$(awk "BEGIN {printf \"%.2f\", $1/$2}")
 
-#check if aspect ratio is x:9
-if [ "$dec" = "0" ]; then
-    #get height in aspect ratio
-    ratio="$((9 * $1/$2))"
+# Define known ratios
+declare -A ratios=(
+    [1.77]="16-9"
+    #[2.33]="21-9"
+    [3.55]="32-9"
+    #[1.33]="4-3"
+    [1.60]="16-10"
+    #[2.10]="21-10"
+    #[3.20]="32-10"
+    #[1.50]="3-2"
+    #[0.56]="9-16"
+    #[0.42]="9-21"
+    #[0.28]="9-32"
+    #[0.75]="3-4"
+    #[0.62]="10-16"
+    #[0.48]="10-21"
+    #[0.31]="10-32"
+    #[0.66]="2-3"
+)
 
-    #check if aspect ratio is 16:9
-    if [ "$ratio" = "16" ]; then
-        echo "$HOME/Pictures/wallpapers/16-9"
-        exit 0
+# Find closest ratio
+closest_key=$(for key in "${!ratios[@]}"; do
+    echo "$key"
+done | awk -v target="$actual_ratio" '{print ($0-target)^2, $0}' | sort -n | head -1 | awk '{print $2}')
 
-    #check if aspect ratio is 21:9
-    elif [ "$ratio" = "21" ]; then
-        echo "$HOME/Pictures/wallpapers/21-9"
-        exit 0
-
-    #check if aspect ratio is 32:9
-    elif [ "$ratio" = "32" ]; then
-        echo "$HOME/Pictures/wallpapers/32-9"
-        exit 0
-
-    #check if aspect ratio is 4:3
-    elif [ "$ratio" = "12" ]; then
-        echo "$HOME/Pictures/wallpapers/4-3"
-        exit 0
-    fi
-
-else
-    value10="$((100 * $1/$2))"
-    dec=${value10: -1}
-    #check if aspect ratio is x:10
-    if [ "$dec" = "0" ]; then
-        #get height in aspect ratio
-        ratio="$((10 * $1/$2))"
-
-        #check if aspect ratio is 16:10
-        if [ "$ratio" = "16" ]; then
-            echo "$HOME/Pictures/wallpapers/16-10"
-            exit 0
-        
-        #check if aspect ratio is 21:10
-        elif [ "$ratio" = "21" ]; then
-            echo "$HOME/Pictures/wallpapers/21-10"
-            exit 0
-
-        #check if aspect ratio is 32:10
-        elif [ "$ratio" = "32" ]; then
-            echo "$HOME/Pictures/wallpapers/32-10"
-            exit 0
-
-        #check if aspect ratio is 3:2
-        elif [ "$ratio" = "15" ]; then
-            echo "$HOME/Pictures/wallpapers/3-2"
-            exit 0        
-        fi
-    fi    
-fi
-
-#----------vertical displays----------
-
-value10="$((90 * $2/$1))"
-dec=${value10: -1}
-
-#check if aspect ratio is 9:x
-if [ "$dec" = "0" ]; then
-    #get width in aspect ratio
-    ratio="$((9 * $2/$1))"
-
-    #check if aspect ratio is 9:16
-    if [ "$ratio" = "16" ]; then
-        echo "$HOME/Pictures/wallpapers/9-16"
-        exit 0
-
-    #check if aspect ratio is 9:21
-    elif [ "$ratio" = "21" ]; then
-            echo "$HOME/Pictures/wallpapers/9-21"
-            exit 0
-
-    #check if aspect ratio is 9:32
-    elif [ "$ratio" = "32" ]; then
-        echo "$HOME/Pictures/wallpapers/9-32"
-        exit 0
-
-    #check if aspect ratio is 3:4
-    elif [ "$ratio" = "12" ]; then
-        echo "$HOME/Pictures/wallpapers/3-4"
-        exit 0    
-    fi
-
-else
-    value10="$((100 * $2/$1))"
-    dec=${value10: -1}
-    #check if aspect ratio is x:10
-    if [ "$dec" = "0" ]; then
-        #get width in aspect ratio
-        ratio="$((10 * $2/$1))"
-
-        #check if aspect ratio is 10:16
-        if [ "$ratio" = "16" ]; then
-            echo "$HOME/Pictures/wallpapers/10-16"
-            exit 0
-
-        #check if aspect ratio is 10:21
-        elif [ "$ratio" = "21" ]; then
-            echo "$HOME/Pictures/wallpapers/10-21"  
-            exit 0
-
-        #check if aspect ratio is 10:32
-        elif [ "$ratio" = "32" ]; then
-            echo "$HOME/Pictures/wallpapers/10-32"
-            exit 0
-
-        #check if aspect ratio is 2:3
-        elif [ "$ratio" = "15" ]; then
-            echo "$HOME/Pictures/wallpapers/2-3"
-            exit 0
-        fi
-    fi    
-fi
-
-#default folder if aspect ratio doesn't match any known ratios
-echo "$HOME/Pictures/wallpapers/16-9"
+echo "$HOME/Pictures/wallpapers/${ratios[$closest_key]}"
 exit 0

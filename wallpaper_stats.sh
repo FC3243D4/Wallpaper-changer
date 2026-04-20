d319b2a081d281722db81e063ca43968e24beda6
FC3243D4 <128162187+FC3243D4@users.github.com>
2026-04-20 17:48:44 +0200

    refined
    added flags and improve output formatting (basically remade from 0)

--- wallpaper_stats.sh
+++ wallpaper_stats.sh
@@ -2,100 +2,212 @@
 
 mode="all"
 dir="."
-serie_mode=0
+individual=0
+top_n=0
+show_percentage=0
+details=0
+details_group=0
 
 # Parse arguments
-for arg in "$@"; do
-    case "$arg" in
+while [[ $# -gt 0 ]]; do
+    case "$1" in
         --nsfw) mode="nsfw" ;;
         --sfw)  mode="sfw" ;;
         --all)  mode="all" ;;
-        --serie) serie_mode=1 ;;
+        --individual) individual=1 ;;
+        --percentage) show_percentage=1 ;;
+        --details) details=1 ;;
+        --details-group) details_group=1 ;;
+        --top)
+            shift
+            top_n="$1"
+            ;;
         --help)
-            echo "Usage: $0 [--nsfw | --sfw | --all] [--serie] [directory]"
+            echo "Usage: $0 [--nsfw|--sfw|--all] [--individual] [--details|--details-group] [--percentage] [--top N] [directory]"
             exit 0
             ;;
         *)
-            dir="$arg"
+            dir="$1"
             ;;
     esac
+    shift
 done
 
-if [ "$serie_mode" -eq 1 ]; then
-    # 📁 SERIES MODE (real file count)
+# mutually exclusive flags
+if [[ "$details" -eq 1 && "$details_group" -eq 1 ]]; then
+    echo "Error: --details and --details-group are mutually exclusive"
+    exit 1
+fi
 
-    if [ "$mode" = "nsfw" ]; then
-        find "$dir" -type f -name "*nsfw*.png"
-    elif [ "$mode" = "sfw" ]; then
-        find "$dir" -type f -name "*.png" ! -name "*nsfw*"
-    else
-        find "$dir" -type f -name "*.png"
-    fi |
-    awk -F/ '
-    {
-        # parent directory = series
-        serie = $(NF-1)
-        count[serie]++
-    }
-    END {
-        for (s in count)
-            print count[s], s
-    }' | sort -nr
+find "$dir" -type f -name "*.png" | awk -F/ -v mode="$mode" -v individual="$individual" '
 
-else
-    # 👤 CHARACTER MODE
+function camel_to_words(str) {
+    out = ""
+    len = length(str)
 
-    find "$dir" -type f -name "*.png" | awk -F/ -v mode="$mode" '
-    {
-        file = $NF
-        sub(/\.png$/, "", file)
+    for (i = 1; i <= len; i++) {
+        c = substr(str, i, 1)
+        prev = (i > 1) ? substr(str, i-1, 1) : ""
+        nxt  = (i < len) ? substr(str, i+1, 1) : ""
 
-        is_nsfw = (file ~ /^nsfw-/)
-
-        sub(/^nsfw-/, "", file)
-
-        if (!match(file, /-([0-9]+)$/, arr)) next
-        n = arr[1] + 0
-
-        name = substr(file, 1, RSTART-1)
-
-        # allow 1–2 parts (handles chi-chi)
-        split(name, parts, "-")
-        if (length(parts) > 2) next
-
-        char = name
-
-        if (is_nsfw) {
-            if (n > max_nsfw[char])
-                max_nsfw[char] = n
+        if (i > 1 &&
+            c ~ /[A-Z]/ &&
+            (prev ~ /[a-z0-9]/ || nxt ~ /[a-z]/)) {
+            out = out "-" tolower(c)
         } else {
-            if (n > max_sfw[char])
-                max_sfw[char] = n
+            out = out tolower(c)
         }
     }
-    END {
-        for (c in max_sfw) {
-            sfw = max_sfw[c] + 0
-            nsfw = max_nsfw[c] + 0
+    return out
+}
 
-            if (mode == "sfw")
-                print sfw, c
-            else if (mode == "nsfw")
-                print nsfw, c
+{
+    file = $NF
+    sub(/\.png$/, "", file)
+
+    # exclude "-group-" files
+    if (file ~ /-group-/) next
+
+    serie = $(NF-1)
+
+    is_nsfw = (file ~ /^nsfw-/)
+
+    if (mode == "nsfw" && !is_nsfw) next
+    if (mode == "sfw" && is_nsfw) next
+
+    sub(/^nsfw-/, "", file)
+
+    if (!match(file, /-([0-9]+)$/, arr)) next
+    n = arr[1] + 0
+
+    names_part = substr(file, 1, RSTART-1)
+    nchars = split(names_part, chars, "-")
+
+    if (individual && nchars > 1) next
+
+    is_group = (nchars > 1)
+
+    for (i = 1; i <= nchars; i++) {
+        char = chars[i]
+        pretty = camel_to_words(char)
+        key = serie "/" pretty
+
+        if (is_group) {
+            if (is_nsfw)
+                group_nsfw[key]++
             else
-                print (sfw + nsfw), c
-        }
-
-        # include characters that exist only in nsfw
-        for (c in max_nsfw) {
-            if (!(c in max_sfw)) {
-                nsfw = max_nsfw[c]
-
-                if (mode == "nsfw")
-                    print nsfw, c
-                else if (mode == "all")
-                    print nsfw, c
+                group_sfw[key]++
+        } else {
+            if (is_nsfw) {
+                if (n > max_nsfw[key])
+                    max_nsfw[key] = n
+            } else {
+                if (n > max_sfw[key])
+                    max_sfw[key] = n
             }
         }
-    }' | sort -nr
-fi
+    }
+}
+
+END {
+    for (k in max_sfw) {
+        sfw_i = max_sfw[k]
+        nsfw_i = max_nsfw[k]
+        sfw_g = group_sfw[k]
+        nsfw_g = group_nsfw[k]
+
+        sfw = sfw_i + sfw_g
+        nsfw = nsfw_i + nsfw_g
+        total = sfw + nsfw
+
+        print total "|" sfw_i "|" nsfw_i "|" sfw_g "|" nsfw_g "|" k
+    }
+
+    for (k in max_nsfw) {
+        if (!(k in max_sfw)) {
+            sfw_i = 0
+            nsfw_i = max_nsfw[k]
+            sfw_g = group_sfw[k]
+            nsfw_g = group_nsfw[k]
+
+            sfw = sfw_i + sfw_g
+            nsfw = nsfw_i + nsfw_g
+            total = sfw + nsfw
+
+            print total "|" sfw_i "|" nsfw_i "|" sfw_g "|" nsfw_g "|" k
+        }
+    }
+
+    for (k in group_sfw) {
+        if (!(k in max_sfw) && !(k in max_nsfw)) {
+            sfw_i = 0
+            nsfw_i = 0
+            sfw_g = group_sfw[k]
+            nsfw_g = group_nsfw[k]
+
+            sfw = sfw_g
+            nsfw = nsfw_g
+            total = sfw + nsfw
+
+            print total "|" sfw_i "|" nsfw_i "|" sfw_g "|" nsfw_g "|" k
+        }
+    }
+}
+' | sort -t'|' -nr -k1,1 | awk -F'|' \
+    -v top="$top_n" \
+    -v showp="$show_percentage" \
+    -v details="$details" \
+    -v dgroup="$details_group" '
+BEGIN {
+    if (dgroup && showp)
+        printf "%-4s %-7s %-7s %-7s %-7s %-7s %-7s %s\n",
+               "#","TOTAL","SFW-I","NSFW-I","SFW-G","NSFW-G","NSFW%","CHARACTER"
+    else if (dgroup)
+        printf "%-4s %-7s %-7s %-7s %-7s %-7s %s\n",
+               "#","TOTAL","SFW-I","NSFW-I","SFW-G","NSFW-G","CHARACTER"
+    else if (details && showp)
+        printf "%-4s %-7s %-7s %-7s %-7s %s\n",
+               "#","TOTAL","SFW","NSFW","NSFW%","CHARACTER"
+    else if (details)
+        printf "%-4s %-7s %-7s %-7s %s\n",
+               "#","TOTAL","SFW","NSFW","CHARACTER"
+    else if (showp)
+        printf "%-4s %-7s %-7s %s\n",
+               "#","TOTAL","NSFW%","CHARACTER"
+    else
+        printf "%-4s %-7s %s\n", "#","TOTAL","CHARACTER"
+}
+
+{
+    total  = $1
+    sfw_i  = $2
+    nsfw_i = $3
+    sfw_g  = $4
+    nsfw_g = $5
+    name   = $6
+
+    sfw = sfw_i + sfw_g
+    nsfw = nsfw_i + nsfw_g
+    percent = (total > 0) ? (nsfw / total * 100) : 0
+
+    if (dgroup && showp)
+        printf "%-4d %-7d %-7d %-7d %-7d %-7d %6.1f%% %s\n",
+               NR,total,sfw_i,nsfw_i,sfw_g,nsfw_g,percent,name
+    else if (dgroup)
+        printf "%-4d %-7d %-7d %-7d %-7d %-7d %s\n",
+               NR,total,sfw_i,nsfw_i,sfw_g,nsfw_g,name
+    else if (details && showp)
+        printf "%-4d %-7d %-7d %-7d %6.1f%% %s\n",
+               NR,total,sfw,nsfw,percent,name
+    else if (details)
+        printf "%-4d %-7d %-7d %-7d %s\n",
+               NR,total,sfw,nsfw,name
+    else if (showp)
+        printf "%-4d %-7d %6.1f%% %s\n",
+               NR,total,percent,name
+    else
+        printf "%-4d %-7d %s\n", NR,total,name
+
+    if (top > 0 && NR >= top)
+        exit
+}'

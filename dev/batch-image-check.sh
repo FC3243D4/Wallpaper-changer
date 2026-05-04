4448dc35dcf952cee62f67223848353b577f1778
FC3243D4 <128162187+FC3243D4@users.github.com>
2026-05-04 13:01:16 +0200

    improved readibility

--- dev/batch-image-check.sh
+++ dev/batch-image-check.sh
@@ -2,9 +2,18 @@
 
 flag=$1
 shift
-for folder in "$@"; do
-    ./dev/check_image.sh "$flag" "$folder"
-    echo ""
-    echo "Finished checking $folder"
+folder=$1
+shift
+for folders in "$@"; do
+    check=$folder$folders
+    echo "Checking $check..."
+    result=$(./dev/check_image.sh "$flag" "$check")
+    if [[ $result == *"|"* ]]; then
+        echo "$result"
+        break
+    else
+        echo ""
+        echo "no issues found in $check"
+    fi
     echo "-----------------------------"
 done
